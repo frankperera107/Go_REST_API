39039767f6da6099931388ef3c5c2867e1608972
Frank Perera <frankperera107@gmail.com>
2024-12-07 11:58:36 +0800

    Added create event and get events endpoints.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/frankperera107/golang/rest_api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,10 +11,30 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
+	server.POST("/events", createEvent)
+
 	server.Run(":8080") //localhost:8080
 }
 
 
 func getEvents(context *gin.Context){
-	context.JSON(http.StatusOK, gin.H{"message":"Hello Go REST API!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context){
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse received data."})
+		return 
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }

5ff36884337bf36abc4bf7c7200e1a9ade02d937
Frank Perera <frankperera107@gmail.com>
2024-12-07 16:17:51 +0800

    Added REST endpoints to create, read events and a SQLite database for data storage.

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/frankperera107/golang/rest_api/db"
 	"github.com/frankperera107/golang/rest_api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
@@ -18,7 +20,11 @@ func main() {
 
 
 func getEvents(context *gin.Context){
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Error occured while retrieving events."})
+		return 
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -34,7 +40,11 @@ func createEvent(context *gin.Context){
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+
+	if err!= nil{
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
